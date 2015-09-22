c9c854a0d272df13aeff1bf3e037af7ed8a9ec06
Ivor Padilla <ivorjpc@gmail.com>
2015-09-22 11:02:43 -0430

    Added Bookmark and Topic

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name: "Cesar"
+    name "Cesar"
     sequence(:email, 100) { |n| "person#{n}@example.com" }
-    password: 'helloworld'
-    password_confirmation: 'helloworld'
+    password 'helloworld'
+    password_confirmation 'helloworld'
   end
 end
