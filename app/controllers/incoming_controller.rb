df5c5004c5440e4575b23c221d3e4161e7d292d3
Ivor Padilla <ivorjpc@gmail.com>
2015-09-23 13:27:48 -0430

    Added the create action for create#incoming

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,13 +4,27 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
 
-    # You put the message-splitting and business
-    # magic here.
+    # Find the user by using params[:sender]
+    @user = User.find(params[:sender])
+    # Find the topic by using params[:subject]
+    @topic = Topic.find(params[:subject])
 
+    # Assign the url to a variable after retreiving it from params["body-plain"]
+    @url = params["body-plain"]
+    # Check if user is nil, if so, create and save a new user
+
+    if @user.nil?
+      @user = User.create(email: params[:sender])
+    end
+
+    # Check if the topic is nil, if so, create and save a new topic
+    if @topic.nil?
+      @topic = Topic.create(title: params[:subject], user_id: @user.id)
+    end
+
+    # Now that you're sure you have a valid user and topic, build and save a new bookmark
+    @bookmark = Bookmark.create(url: @url, topic_id: @topic.id)
     # Assuming all went well.
     head 200
   end
