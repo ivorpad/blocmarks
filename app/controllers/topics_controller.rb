b656e5b75a95d340618bdddf6debac05536f3e33
Ivor Padilla <ivorjpc@gmail.com>
2015-09-26 19:21:03 -0430

    Delete implemented

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,7 @@
 class TopicsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
     @topics = Topic.all
   end
@@ -7,8 +10,20 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
   end
 
   def edit
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "The topic has been deleted."
+      redirect_to @topic
+    else
+      flash[:error] = "The topic cannot be deleted."
+    end
   end
 end
