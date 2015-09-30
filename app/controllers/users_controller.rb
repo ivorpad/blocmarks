6fb92e8bd50bff60169f40e44db643e1e3201dc6
Ivor Padilla <ivorjpc@gmail.com>
2015-09-30 12:28:01 -0430

    Finished User Profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user_bookmarks = current_user.bookmarks
+    @liked_bookmarks = current_user.likes.where(user_id: current_user.id)
   end
 end
