dcb3a8b0e44909e24a1380c7b25089c8ba20f042
Ivor Padilla <ivorjpc@gmail.com>
2015-09-30 11:49:58 -0430

    Added the necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :bookmarks, except: :index
 
   devise_for :users
+  resources :users, only: [:show]
+
   root to: "welcome#index"
 
   post :incoming, to: 'incoming#create'
